7204d6cee51b511d735fe401bfcd3c6fb1cfffe9
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-08-28 01:20:31 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,20 @@ import app from "./app.js";
 import cors from "cors";
 
 // Allow all origins (not recommended for production)
-app.use(cors());
+// app.use(cors());
 
-// Or, allow specific origin
+// Allow specific origin and handle preflight requests
 app.use(
   cors({
     origin: "https://food-order-pk78.netlify.app",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
   })
 );
+
+// Explicitly handle preflight requests
+app.options("*", cors());
 
 const PORT = 3200;
 app.listen(PORT, function (err) {
