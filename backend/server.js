e001beeed5fb89472963d25db0d9ad03aba6a9cc
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-08-28 01:05:59 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
-import app from './app.js';
+import app from "./app.js";
+import cors from "cors";
 
+// Allow all origins (not recommended for production)
+app.use(cors());
 
+// Or, allow specific origin
+app.use(
+  cors({
+    origin: "https://food-order-pk78.netlify.app",
+  })
+);
 
-
-const PORT=3200;
-app.listen(PORT, function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", PORT);
+const PORT = 3200;
+app.listen(PORT, function (err) {
+  if (err) console.log("Error in server setup");
+  console.log("Server listening on Port", PORT);
 });
--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -5,7 +5,6 @@
 // import toast from 'react-hot-toast';
 // import { useNavigate } from 'react-router-dom';
 
-
 // const Reservation = () => {
 
 //     const[firstName, setFirstName]= useState("");
@@ -46,7 +45,7 @@
 
 //     // const handleReservation = async (e) => {
 //     //     e.preventDefault();
-    
+
 //     //     try {
 //     //         const response = await axios.post(
 //     //             "http://localhost:3200/api/v1/reservation/send",
@@ -58,7 +57,7 @@
 //     //                 withCredentials: true
 //     //             }
 //     //         );
-    
+
 //     //         if (response.data) {
 //     //             toast.success(response.data.message);
 //     //             setFirstName("");
@@ -96,7 +95,7 @@
 //                     <div>
 //                         <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
 //                         <input type="text" placeholder='Last  Name' value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                        
+
 //                     </div>
 //                     <div>
 //                         <input type="date" placeholder='Date' value={date} onChange={(e) => setDate(e.target.value)} />
@@ -105,7 +104,7 @@
 //                     <div>
 //                         <input type="email" placeholder='Email' value={email} className='email_tag' onChange={(e) => setEmail(e.target.value)} />
 //                         <input type="number" placeholder='Phone' value={phone} onChange={(e) => setPhone(e.target.value)} />
-                    
+
 //                     </div>
 //                     <button type='submit' >Reserve Now
 //                      {/* {" "} <span>
@@ -120,87 +119,121 @@
 
 // export default Reservation;
 
-
-
-
-
-
-import React, { useState } from 'react';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Reservation = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
-    const [phone, setPhone] = useState("");
-    const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [phone, setPhone] = useState("");
+  const navigate = useNavigate();
 
-    const handleReservation = async (e) => {
-        e.preventDefault();
+  const handleReservation = async (e) => {
+    e.preventDefault();
 
-        try {
-            const { data } = await axios.post("http://localhost:3200/api/v1/reservation/send", {
-                firstName,
-                lastName,
-                email,
-                date,
-                time,
-                phone
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                withCredentials: true
-            });
-            toast.success(data.message);
-            setFirstName("");
-            setLastName("");
-            setEmail("");
-            setDate("");
-            setTime("");
-            setPhone("");
-            navigate("/success");
-        } catch (error) {
-            // Extract a meaningful message from the error object
-            const errorMessage = error.response?.data?.message || error.message || "An error occurred";
-            toast.error(errorMessage);
+    const Url = "https://food-order-2j5k.onrender.com";
+    // const Url = "http://localhost:3200";
+
+    try {
+      const { data } = await axios.post(
+        Url + "/api/v1/reservation/send",
+        {
+          firstName,
+          lastName,
+          email,
+          date,
+          time,
+          phone,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
         }
+      );
+      toast.success(data.message);
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setDate("");
+      setTime("");
+      setPhone("");
+      navigate("/success");
+    } catch (error) {
+      // Extract a meaningful message from the error object
+      const errorMessage =
+        error.response?.data?.message || error.message || "An error occurred";
+      toast.error(errorMessage);
     }
+  };
 
-    return (
-        <section className='reservation' id='reservation'>
-            <div className="container">
-                <div className="banner">
-                    <img src="reservation.png" alt="reservation" />
-                </div>
-                <div className="banner">
-                    <div className="reservation_form_box">
-                        <h1>Make a reservation</h1>
-                        <p>For Further Questions, Please Call</p>
-                        <form onSubmit={handleReservation}>
-                            <div>
-                                <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                                <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                            </div>
-                            <div>
-                                <input type="date" placeholder='Date' value={date} onChange={(e) => setDate(e.target.value)} />
-                                <input type="time" placeholder='Time' value={time} onChange={(e) => setTime(e.target.value)} />
-                            </div>
-                            <div>
-                                <input type="email" placeholder='Email' value={email} className='email_tag' onChange={(e) => setEmail(e.target.value)} />
-                                <input type="number" placeholder='Phone' value={phone} onChange={(e) => setPhone(e.target.value)} />
-                            </div>
-                            <button type='submit'>Reserve Now</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="reservation" id="reservation">
+      <div className="container">
+        <div className="banner">
+          <img src="reservation.png" alt="reservation" />
+        </div>
+        <div className="banner">
+          <div className="reservation_form_box">
+            <h1>Make a reservation</h1>
+            <p>For Further Questions, Please Call</p>
+            <form onSubmit={handleReservation}>
+              <div>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="date"
+                  placeholder="Date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+                <input
+                  type="time"
+                  placeholder="Time"
+                  value={time}
+                  onChange={(e) => setTime(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  className="email_tag"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="number"
+                  placeholder="Phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <button type="submit">Reserve Now</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Reservation;
